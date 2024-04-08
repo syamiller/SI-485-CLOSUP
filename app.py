dc3f93ee17eb2e54cdaa020064db0f23bfdc554d
Simon Miller <symiller@umich.edu>
2024-04-08 11:42:24 -0400

    done

---
+++ app.py
@@ -0,0 +1,221 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import pandas as pd
+import xbrl_functions
+
+#load data
+report_ids = [
+    '677268',
+    '677267'
+]
+
+df = pd.read_csv('xbrl_data.csv')
+df['fact.value'] = pd.to_numeric(df['fact.value'], errors='coerce')
+df = df[df['fact.value'].notna()]
+
+final_ratios = pd.DataFrame(columns= ['report_id', 'report_entity_name', 'ratio', 'value', 'green_start', 'green_end', 'yellow_start', 'yellow_end', 'red_start', 'red_end', 'var_1_name', 'var_1_value', 'var_2_name', 'var_2_value'])
+
+for report_id in report_ids:
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_short_run(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_days_cash_on_hand(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_liquidity(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_gov_debt_coverage(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_expenditure_per_capita(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_net_asset_growth(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_captial_asset_ga(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+    final_ratios = pd.concat([final_ratios, pd.DataFrame([xbrl_functions.get_captial_asset_bta(df, report_id)], columns=final_ratios.columns)], ignore_index=True)
+print(final_ratios)
+
+
+# Define the Dash app
+app = dash.Dash(__name__)
+# Define the layout of your dashboard
+app.layout = html.Div([
+    html.H1("Government Financial Ratios", style={'textAlign': 'center'}),
+    dcc.Dropdown(
+        [value for value in final_ratios['report_entity_name'].unique()],
+        'County of Ogemaw',
+        id='report-dropdown',
+        clearable=False,
+        style={'width': '50%', 'margin': 'auto', 'textAlign': 'center'}
+    ),
+    
+    html.Div([    
+        html.Div(children=[
+            dcc.Graph(id='gauge-plot1', mathjax=True),
+            dcc.Markdown(id='gauge-plot1-text', mathjax=True),
+            html.Br(),
+            dcc.Graph(id='gauge-plot2', mathjax=True),
+            dcc.Markdown(id='gauge-plot2-text', mathjax=True),
+            html.Br(),
+            dcc.Graph(id='gauge-plot3', mathjax=True),
+            dcc.Markdown(id='gauge-plot3-text', mathjax=True),
+            html.Br(),
+            dcc.Graph(id='gauge-plot4', mathjax=True),
+            dcc.Markdown(id='gauge-plot4-text', mathjax=True),
+        ], style={'padding': 10, 'flex': 1}),
+
+        html.Div(children=[
+            dcc.Graph(id='gauge-plot5', mathjax=True),
+            dcc.Markdown(id='gauge-plot5-text', mathjax=True),
+            html.Br(),
+            dcc.Graph(id='gauge-plot6', mathjax=True),
+            dcc.Markdown(id='gauge-plot6-text', mathjax=True),
+            html.Br(),
+            dcc.Graph(id='gauge-plot7', mathjax=True),
+            dcc.Markdown(id='gauge-plot7-text', mathjax=True),
+            html.Br(),
+            dcc.Graph(id='gauge-plot8', mathjax=True),
+            dcc.Markdown(id='gauge-plot8-text', mathjax=True),
+        ], style={'padding': 10, 'flex': 1})
+    ], style={'display': 'flex', 'flexDirection': 'row'})
+])
+
+def create_gauge(value, ratio):
+    data = final_ratios[final_ratios['report_entity_name'] == value].iloc[ratio]
+    fig = go.Figure(go.Indicator(mode="gauge+number+delta",
+        value=round(data['value'],2),
+        domain={'x': [0, 1], 'y': [0, 1]},
+        title={'text': data['ratio'], 'font': {'size': 24}},
+        delta={'reference': data['green_start'], 'increasing': {'color': "RebeccaPurple"}},
+        gauge={
+            'axis': {'range': [None, max([data['green_end'], data['yellow_end'], data['red_end']])], 'tickwidth': 1, 'tickcolor': "darkblue"},
+            'bar': {'color': "black"},
+            'bgcolor': "white",
+            'borderwidth': 2,
+            'bordercolor': "gray",
+            'steps': [
+                {'range': [data['red_start'], data['red_end']], 'color': 'crimson'},  # Less than 0.15, red
+                {'range': [data['green_start'], data['green_end']], 'color': 'mediumseagreen'},         # Between 0.15 and 0.2, green
+                {'range': [data['yellow_start'], data['yellow_end']], 'color': 'gold'}], # Above 0.2, green
+            'threshold': {
+                'line': {'color': "black", 'width': 4},
+                'thickness': 0.75,
+                'value': round(data['value'],2)}}))
+
+    fig.update_layout(paper_bgcolor="lavender", font={'color': "black", 'family': "Arial"})
+
+    return fig
+
+def update_markdown(value, ratio):
+    data = final_ratios[final_ratios['report_entity_name'] == value].iloc[ratio]
+    return r"""$\text{{Formula}} = \frac{{\text{{ {} }}}}{{\text{{ {} }}}} = \frac{{\text{{ {} }}}}{{\text{{ {} }}}}$""".format(data['var_1_name'], data['var_2_name'], round(data['var_1_value'], 2), round(data['var_2_value'], 2))
+
+@app.callback(
+    Output('gauge-plot1', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot1(value):
+    return create_gauge(value, 0)
+
+@app.callback(
+    Output('gauge-plot2', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot2(value):
+    return create_gauge(value, 1)
+
+@app.callback(
+    Output('gauge-plot3', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot3(value):
+    return create_gauge(value, 2)
+
+@app.callback(
+    Output('gauge-plot4', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot4(value):
+    return create_gauge(value, 3)
+
+@app.callback(
+    Output('gauge-plot5', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot5(value):
+    return create_gauge(value, 4)
+
+@app.callback(
+    Output('gauge-plot6', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot6(value):
+    return create_gauge(value, 5)
+
+@app.callback(
+    Output('gauge-plot7', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot7(value):
+    return create_gauge(value, 6)
+
+@app.callback(
+    Output('gauge-plot8', 'figure'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot8(value):
+    return create_gauge(value, 7)
+
+@app.callback(
+    Output('gauge-plot1-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot1_text(value):
+    return update_markdown(value, 0)
+
+@app.callback(
+    Output('gauge-plot2-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot2_text(value):
+    return update_markdown(value, 1)
+
+@app.callback(
+    Output('gauge-plot3-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot3_text(value):
+    return update_markdown(value, 2)
+
+@app.callback(
+    Output('gauge-plot4-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot4_text(value):
+    return update_markdown(value, 3)
+
+@app.callback(
+    Output('gauge-plot5-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot5_text(value):
+    return update_markdown(value, 4)
+
+@app.callback(
+    Output('gauge-plot6-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot6_text(value):
+    return update_markdown(value, 5)
+
+@app.callback(
+    Output('gauge-plot7-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot7_text(value):
+    return update_markdown(value, 6)
+
+@app.callback(
+    Output('gauge-plot8-text', 'children'),
+    [Input('report-dropdown', 'value')]
+)
+def update_gauge_plot8_text(value):
+    return update_markdown(value, 7)
+    
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=False, host='0.0.0.0', port = 8080)
---
+++ xbrl_functions.py
@@ -0,0 +1,114 @@
+def get_short_run(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+    balance_unassigned = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'FundBalanceUnassigned') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']
+    fund_revenue = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'RevenuesModifiedAccrual') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']    
+
+    ratio = balance_unassigned / fund_revenue
+    return {'report_id': report_id, 'report_entity_name' : report_entity, 'ratio': 'Short Run Financial Position', 'value': ratio, 'green_start': 0.15, 
+            'green_end': 0.2, 'yellow_start': 0.2, 'yellow_end': 1, 'red_start': 0, 'red_end': 0.15, 
+            'var_1_name':'General Fund Balance Unassigned', 'var_1_value':balance_unassigned, 
+            'var_2_name':'General Fund Revenue', 'var_2_value': fund_revenue}
+    
+def get_days_cash_on_hand(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+    cash_and_cash_equivalents = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'CashAndCashEquivalentsModifiedAccrual') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']
+    expenditures = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'ExpendituresModifiedAccrual') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']
+    
+    ratio = cash_and_cash_equivalents / (expenditures / 365)
+    return {'report_id': report_id, 'ratio': 'Days of Cash on Hand', 'value': ratio, 'green_start': 125, 
+            'green_end': 500, 'yellow_start': 90, 'yellow_end': 125, 'red_start': 0, 'red_end': 90,
+            'var_1_name':'Cash and Cash Equivalents', 'var_1_value':cash_and_cash_equivalents, 
+            'var_2_name':'Expenditures / 365', 'var_2_value':expenditures/365, 'report_entity_name' : report_entity}
+    
+def get_liquidity(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+    cash_and_cash_equivalents = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'CashAndCashEquivalentsModifiedAccrual') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']
+    liabilities = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'LiabilitiesModifiedAccrual') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']
+    deferred_revenue = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'DeferredInflowsOfResourcesModifiedAccrual') & (df['fund-member'] == 'GeneralFundMember')].iloc[0]['fact.value']
+    
+    ratio = cash_and_cash_equivalents / (liabilities - deferred_revenue)
+    return {'report_id': report_id, 'ratio': 'Liquidity', 'value': ratio, 'green_start': 2, 'green_end': 10, 
+            'yellow_start': 1, 'yellow_end': 2, 'red_start': 0, 'red_end': 1,
+            'var_1_name':'Cash and Cash Equivalents', 'var_1_value':cash_and_cash_equivalents,
+            'var_2_name':'Liabilities - Deferred Revenue', 'var_2_value': liabilities-deferred_revenue, 'report_entity_name' : report_entity}
+    
+def get_gov_debt_coverage(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+    debt_serv_expenditures = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'DebtServicePrincipalRepaymentModifiedAccrual') & (df['fund-member'] == 'GovernmentalFundsMember')].iloc[0]['fact.value'] \
+        + df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'DebtServiceInterestAndFiscalChargesModifiedAccrual') & (df['fund-member'] == 'GovernmentalFundsMember')].iloc[0]['fact.value']
+    total_expenditures = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'ExpendituresModifiedAccrual') & (df['fund-member'] == 'GovernmentalFundsMember')].iloc[0]['fact.value']
+    try:
+        capital_outlay = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'ExpendituresForCapitalOutlayModifiedAccrual') & (df['fund-member'] == 'GovernmentalFundsMember')].iloc[0]['fact.value']
+    except:
+        capital_outlay = 0
+
+    ratio = debt_serv_expenditures / (total_expenditures - capital_outlay - debt_serv_expenditures)
+    return {'report_id': report_id, 'ratio': 'Government Debt Coverage', 'value': ratio, 'green_start': 0, 
+        'green_end': 0.1, 'yellow_start': 0.1, 'yellow_end': 0.15, 'red_start': 0.15, 'red_end': 1,
+        'var_1_name':'Debt Service Expenditures', 'var_1_value':debt_serv_expenditures, 
+        'var_2_name':'Total Expenditures - Capital Outlay - Debt Service Expenditures', 'var_2_value':total_expenditures - capital_outlay - debt_serv_expenditures, 'report_entity_name' : report_entity}
+        
+def get_expenditure_per_capita(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+    total_expenditures = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'ExpendituresModifiedAccrual') & (df['fund-member'] == 'GovernmentalFundsMember')].iloc[0]['fact.value']
+    if report_id == 677268:
+        population = 20726
+    else:
+        population = 83300
+    ratio = total_expenditures / population
+    return {'report_id': report_id, 'ratio': 'Expenditure per Capita', 'value': ratio, 'green_start': 500, 
+        'green_end': 1200, 'yellow_start':100 , 'yellow_end': 100, 'red_start': 0, 'red_end': 500,
+        'var_1_name':'Total Expenditures', 'var_1_value':total_expenditures, 
+        'var_2_name':'Population',
+        'var_2_value':population, 'report_entity_name': report_entity}
+    
+def get_net_asset_growth(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+    change_net_position = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'ChangesInNetPosition') & (df['fund-member'] == 'GovernmentalActivitiesMember')].iloc[0]['fact.value']
+    begin_net_position = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'NetPositionAtBeginningOfPeriodAfterAdjustments') & (df['fund-member'] == 'GovernmentalActivitiesMember')].iloc[0]['fact.value']
+    ratio = -change_net_position / begin_net_position
+    return {'report_id': report_id, 'ratio': 'Net Asset Growth', 'value': ratio, 'green_start': 0.02, 
+        'green_end': 0.5, 'yellow_start': 0.0, 'yellow_end': 0.02, 'red_start': -0.5, 'red_end': 0.0,
+        'var_1_name':'Governmental Activities Change in Net Position', 'var_1_value':change_net_position, 
+        'var_2_name':'Governmental Activities Beginning Net Position',
+        'var_2_value':begin_net_position, 'report_entity_name': report_entity}
+    
+    
+def get_captial_asset_ga(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+
+    beginning_net_value = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'CapitalAssetsNetOfAccumulatedDepreciationAndAmortization') & (df['fund-member'] == 'GovernmentalActivitiesMember')].iloc[:2].sort_values(by='period.fiscal-year').iloc[0]['fact.value']
+    end_net_value = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'CapitalAssetsNetOfAccumulatedDepreciationAndAmortization') & (df['fund-member'] == 'GovernmentalActivitiesMember')].iloc[:2].sort_values(by='period.fiscal-year').iloc[1]['fact.value']
+
+    captial_asset_ga = (end_net_value - beginning_net_value) / beginning_net_value
+    captial_asset_ga
+
+    return {
+        'report_id': report_id, 
+        'ratio': 'Capital Asset Condition (Governmental Activities)', 'value': captial_asset_ga, 
+        'green_start': 0.02, 'green_end': 1, 
+        'yellow_start': 0, 'yellow_end': 0.02,
+        'red_start': -1, 'red_end': 0, 
+        'var_1_name':'Ending Net Value - Beginning Net Value', 'var_1_value': (end_net_value - beginning_net_value), 
+        'var_2_name':'Beginning Net Value', 'var_2_value': beginning_net_value, 'report_entity_name': report_entity
+    }
+    
+
+def get_captial_asset_bta(df, report_id):
+    report_entity = df[df['report.id'] == int(report_id)].iloc[0]['report.entity-name']
+
+    beginning_net_value = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'CapitalAssetsNetOfAccumulatedDepreciationAndAmortization') & (df['fund-member'] == 'BusinessTypeActivitiesMember')].iloc[:2].sort_values(by='period.fiscal-year').iloc[0]['fact.value']
+    end_net_value = df[(df['report.id'] == int(report_id)) & (df['cube.primary-local-name'] == 'CapitalAssetsNetOfAccumulatedDepreciationAndAmortization') & (df['fund-member'] == 'BusinessTypeActivitiesMember')].iloc[:2].sort_values(by='period.fiscal-year').iloc[1]['fact.value']
+
+    captial_asset_bta = (end_net_value - beginning_net_value) / beginning_net_value
+    captial_asset_bta
+
+    return {
+        'report_id': report_id,
+        'ratio': 'Capital Asset Condition (Business Type Activities)', 'value': captial_asset_bta,
+        'green_start': 0.02, 'green_end': 1,
+        'yellow_start': 0, 'yellow_end': 0.02,
+        'red_start': -1, 'red_end': 0, 
+        'var_1_name':'Ending Net Value - Beginning Net Value', 'var_1_value': (end_net_value - beginning_net_value), 
+        'var_2_name':'Beginning Net Value', 'var_2_value': beginning_net_value, 'report_entity_name': report_entity
+    }
